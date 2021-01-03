116b46439eb7f5263394d368529c7a3546fcffc0
Hiago W Petris <hiago.petris@gmail.com>
2021-01-03 15:38:00 -0300

    Tela inicial

--- app/src/main/java/com/example/finapp/activity/MainActivity.java
+++ app/src/main/java/com/example/finapp/activity/MainActivity.java
@@ -5,15 +5,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.example.finapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imgCadastrar, imgExtrato, imgPesquisar, imgCategoria;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imgCadastrar = findViewById(R.id.imageViewCadastrar);
+        imgExtrato = findViewById(R.id.imageViewExtrato);
+        imgPesquisar = findViewById(R.id.imageViewPesquisar);
+        imgCategoria = findViewById(R.id.imageViewCategorias);
+
+        imgCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cadastrar(view);
+            }
+        });
+
+        imgExtrato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                extrato(view);
+            }
+        });
+
+        imgPesquisar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        imgCategoria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 
     public void cadastrar(View view){
